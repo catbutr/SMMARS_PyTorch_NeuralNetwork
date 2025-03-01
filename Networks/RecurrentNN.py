ea86e1354e65f418c5d51f0bff888522f530223c
catbutr <pkarabatov@gmail.com>
2025-03-01 23:54:13 +0700

    Some work

--- Networks/RecurrentNN.py
+++ Networks/RecurrentNN.py
@@ -1,6 +1,8 @@
 import torch.nn as nn
 import Enums.ActivationFunctionEnum as af
 import Functions.ChooseActivationFunction as caf
+import torch
+from torch.autograd import Variable 
 
 class extract_tensor(nn.Module):
     def forward(self,x):
@@ -10,6 +12,8 @@ class extract_tensor(nn.Module):
 class ReccurentNN(nn.Module):
     def __init__(self, inputSize, hiddenSize, outputSize, numberOfLayers, activationFunction = af.ActivationFunctionEnum.Tahn):
         super().__init__()
+        self.hidden_size = hiddenSize
+        self.num_layers = numberOfLayers
         stack = []
         stack.append(nn.LSTM(input_size=inputSize,hidden_size=hiddenSize,num_layers=numberOfLayers, batch_first=True))
         stack.append(extract_tensor())
@@ -19,7 +23,13 @@ class ReccurentNN(nn.Module):
         self.layers = nn.ModuleList(seq_stack)
 
     def forward(self, x):
-        out = self.layers[0](x)
+        h_0 = Variable(torch.zeros(self.num_layers, x.size(0), self.hidden_size)) #hidden state
+        c_0 = Variable(torch.zeros(self.num_layers, x.size(0), self.hidden_size)) #internal state
+        # Propagate input through LSTM
+        _, (hn, _) = self.layers[0](x, (h_0, c_0)) #lstm with input, hidden, and internal state
+        hn = hn.view(-1, self.hidden_size) #reshaping the data for Dense layer next
+        out = self.layers[-1](hn)
+        out = self.layers[1](x)
         for layer in self.layers[1:]:
             out = layer(out)
         return out
