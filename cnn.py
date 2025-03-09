58625c2b49aac72ed86426a24b697915f4665e89
catbutr <pkarabatov@gmail.com>
2025-03-09 20:27:26 +0700

    Модель обучается

--- cnn.py
+++ cnn.py
@@ -9,10 +9,7 @@ from sklearn.preprocessing import StandardScaler
 from torch.utils.data import Dataset
 import numpy as np
 from numpy import zeros, newaxis
-class BostonDataset(torch.utils.data.Dataset):
-  '''
-  Prepare the Boston dataset for regression
-  '''
+class CNNDataset(torch.utils.data.Dataset):
 
   def __init__(self, X, y, scale_data=False):
     if not torch.is_tensor(X) and not torch.is_tensor(y):
@@ -30,25 +27,23 @@ class BostonDataset(torch.utils.data.Dataset):
 class ConvNN(nn.Module):
     def __init__(self):
          super(ConvNN, self).__init__() 
-         self.layer1 = nn.Sequential( nn.Conv1d(10, 32, kernel_size=5, stride=1, padding=2), 
-            nn.ReLU()) 
-         self.layer2 = nn.Sequential( nn.Conv1d(32, 64, kernel_size=5, stride=1, padding=2), 
-            nn.ReLU()) 
+         self.layer1 = nn.Sequential( nn.Conv1d(4, 64, kernel_size=5, stride=1, padding=2), 
+            nn.ReLU(), nn.MaxPool1d(1,1)) 
+         self.layer2 = nn.Sequential( nn.Conv1d(64, 32, kernel_size=5, stride=1, padding=2), 
+            nn.ReLU(), nn.MaxPool1d(1,1)) 
          self.drop_out = nn.Dropout() 
-         self.fc1 = nn.Linear(4, 10) 
-         self.fc2 = nn.Linear(10, 1)
+         self.out = nn.Linear(32, 1) 
 
     def forward(self, x):
       out = self.layer1(x) 
       out = self.layer2(out) 
       out = out.reshape(out.size(0), -1) 
       out = self.drop_out(out) 
-      out = self.fc1(out) 
-      out = self.fc2(out) 
+      out = self.out(out) 
       return out
 
 num_epochs = 5 
-num_classes = 4 
+
 batch_size = 100 
 learning_rate = 0.001
 trans = transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.1307,), (0.3081,))]) 
@@ -63,42 +58,54 @@ x = data[feature_list].to_numpy()
 y = data['Kc'].to_numpy()
 # Разделение данных на обучающую и тестовую выборки
 x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=42) 
-#x_train = x_train[:, :, newaxis]
+x_train = x_train[:, :, newaxis]
+x_test = x_test[:, :, newaxis]
 print(np.shape(x_train))
-#print("Среднее значение после нормализации:", x_train.mean(axis=0)) # должно быть близкое к 0
-#print("Стандартное отклонение после нормализации:", x_train.std(axis=0)) # должно быть близкое к 1
-#train_dataset = TensorDataset(torch.from_numpy(x_train).type(torch.float),torch.from_numpy(y_train.values).type(torch.float))
-train_dataset = BostonDataset(x_train, y_train)
-test_dataset = BostonDataset(x_test,y_test)
+train_dataset = CNNDataset(x_train, y_train)
+test_dataset = CNNDataset(x_test,y_test)
 # Загрузка данных
-train_loader = DataLoader(train_dataset,batch_size=10, shuffle=True)
-test_loader = DataLoader(test_dataset,batch_size=10, shuffle=True)
+train_loader = DataLoader(train_dataset,batch_size=batch_size, shuffle=True)
+test_loader = DataLoader(test_dataset,batch_size=batch_size, shuffle=True)
 
 model = ConvNN()
-criterion = nn.BCELoss()
+criterion = nn.BCEWithLogitsLoss()
 optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
-total_step = len(train_loader)
-loss_list = []
-acc_list = []
-for epoch in range(num_epochs):
-    for i, (images, labels) in enumerate(train_loader):
-        # Прямой запуск
-        outputs = model(images)
-        loss = criterion(outputs, labels)
-        loss_list.append(loss.item())
-
-        # Обратное распространение и оптимизатор
+for epoch in range(num_epochs): # 20 epochs at maximum
+    # Print epoch
+    print(f'Starting epoch {epoch+1}')
+    # Set current loss value
+    current_loss = 0.0
+    for i, data in enumerate(train_loader, 0):
+    # Get and prepare inputs
+        inputs, targets = data
+        inputs, targets = inputs.float(), targets.float()
+        targets = targets.reshape((targets.shape[0], 1))
+        # Zero the gradients
         optimizer.zero_grad()
+        # Perform forward pass
+        outputs = model(inputs)
+
+        # Compute loss
+        loss = criterion(outputs, targets)
+        # Perform backward pass
         loss.backward()
+        # Perform optimization
         optimizer.step()
+        # Print statistics
+        # current_loss += loss.item()
+        if i % 10 == 0:
+            print('Loss after mini-batch %5d: %.3f' %
+                  (i + 1, current_loss / 500))
+            current_loss = 0.0
 
-        # Отслеживание точности
-        total = labels.size(0)
+model.eval()
+with torch.no_grad():
+    correct = 0
+    total = 0
+    for images, labels in test_loader:
+        outputs = model(images)
         _, predicted = torch.max(outputs.data, 1)
-        correct = (predicted == labels).sum().item()
-        acc_list.append(correct / total)
+        total += labels.size(0)
+        correct += (predicted == labels).sum().item()
 
-        if (i + 1) % 100 == 0:
-            print('Epoch [{}/{}], Step [{}/{}], Loss: {:.4f}, Accuracy: {:.2f}%'
-                  .format(epoch + 1, num_epochs, i + 1, total_step, loss.item(),
-                          (correct / total) * 100))
+    print('Точность: {} %'.format((correct / total) * 100))
