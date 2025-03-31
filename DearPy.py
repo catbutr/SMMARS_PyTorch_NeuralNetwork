45d26c3be96ab46b7df3850a0cb99fde85df20df
catbutr <68949616+catbutr@users.noreply.github.com>
2025-03-31 22:44:22 +0700

    FileReader

--- DearPy.py
+++ DearPy.py
@@ -12,9 +12,21 @@ def delink_callback(sender, app_data):
     # app_data -> link_id
     dpg.delete_item(app_data)
 
-with dpg.window(label="Tutorial", width=400, height=400):
+def callback(sender, app_data, user_data):
+    print("Sender: ", sender)
+    print("App Data: ", app_data)
 
+with dpg.file_dialog(directory_selector=False, show=False, callback=callback, id="file_dialog_id", width=700 ,height=400):
+    dpg.add_file_extension(".csv", color=(255, 0, 255, 255), custom_text="[CSV]")
+    dpg.add_file_extension(".xlsx", color=(0, 255, 0, 255), custom_text="[Excel]")
+
+with dpg.window(label="Tutorial", width=400, height=400):
     with dpg.node_editor(callback=link_callback, delink_callback=delink_callback):
+        #Блок чтения файла
+        with dpg.node(label="File Reader"):
+            with dpg.node_attribute(label="Output", attribute_type=dpg.mvNode_Attr_Output):
+                dpg.add_button(label="File Selector", callback=lambda: dpg.show_item("file_dialog_id"))
+        #Блок нейронной сети
         with dpg.node(label="FeedForward NN"):
             with dpg.node_attribute(label="Input"):
                 dpg.add_input_int(label="Input size", width=150)
@@ -22,7 +34,9 @@ with dpg.window(label="Tutorial", width=400, height=400):
                 dpg.add_input_int(label="Hidden size", width=150)
                 dpg.add_input_int(label="Output size", width=150)
                 dpg.add_input_int(label="Number of layers", width=150)
-                dpg.add_combo(label="Activation Function", items=['ReLU','LReLU','Sigmoid','Tahn','SoftMax'])
+            with dpg.node_attribute(label="Output", attribute_type=dpg.mvNode_Attr_Output):
+                dpg.add_combo(label="Activation Function", items=['ReLU','LReLU','Sigmoid','Tahn','SoftMax'], width=150)
+
 
 
 dpg.create_viewport(title='Custom Title', width=800, height=600)
